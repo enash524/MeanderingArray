3929e9f9211dc4693487ca0e7a695ae3d241cc1f
Eric Nash <enash524@gmail.com>
2020-03-23 09:38:04 -0500

    1) Updated removal of min/max elements from list

--- src/MeanderingArray.ConsoleApp/Result.cs
+++ src/MeanderingArray.ConsoleApp/Result.cs
@@ -8,30 +8,27 @@ namespace MeanderingArray.ConsoleApp
         public static List<int> MeanderingArray(List<int> unsorted)
         {
             List<int> result = new List<int>();
+
             unsorted.Sort();
-
             result.Add(unsorted[unsorted.Count - 1]);
-            unsorted.RemoveAt(unsorted.Count - 1);
-
             result.Add(unsorted[0]);
-            unsorted.RemoveAt(0);
+            int[] temp = unsorted.GetRange(1, unsorted.Count - 2).Distinct().ToArray();
 
-            unsorted = unsorted.Distinct().ToList();
-            int count = unsorted.Count;
+            int count = temp.Length;
             int[] output = new int[count];
             int idx = 0;
 
-            for (int i = 0, j = unsorted.Count - 1; i <= count / 2 || j > count / 2; i++, j--)
+            for (int i = 0, j = count - 1; i <= count / 2 || j > count / 2; i++, j--)
             {
                 if (idx < count)
                 {
-                    output[idx] = unsorted[j];
+                    output[idx] = temp[j];
                     idx++;
                 }
 
                 if (idx < count)
                 {
-                    output[idx] = unsorted[i];
+                    output[idx] = temp[i];
                     idx++;
                 }
             }
